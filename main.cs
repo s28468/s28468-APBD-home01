4cc3762344aaf615d1f2b63d8a3f3edb482afea5
s28468 <s28468@pjwstk.edu.pl>
2024-03-10 11:34:06 +0100

    Avg commit

--- main.cs
+++ main.cs
@@ -1,3 +1,22 @@
 ﻿var a = "1";
 var a = "2";
 var a = "3";
+using System;
+
+public static class ArrayUtils
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        long sum = 0; // Use long to avoid overflow if dealing with large sums
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return (double)sum / numbers.Length;
+    }
+}
